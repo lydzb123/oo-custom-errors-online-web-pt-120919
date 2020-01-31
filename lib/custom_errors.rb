d7bcbb3235c09e8c82bdabc976ecc7c1d30c92d5
Lydia B <lydiabaik@gmail.com>
2020-01-31 04:53:53 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -15,11 +15,15 @@ class Person
   def get_married(person)
       self.partner = person
       if person.class != Person
-        raise PartnerError
+        begin
+          raise PartnerError
+        rescue PartnerError => error
+            puts error.message
+        end
       else
         person.partner = self
       end
-    end
+  end
 
 
 end
